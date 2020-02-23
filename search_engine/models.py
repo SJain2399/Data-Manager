b5e3f67db649cc90212be561f094c49f53046f24
Gaurav Modi <gauravmodi18052000@gmail.com>
2020-02-23 19:29:33 +0530

    Added Search Engine

--- search_engine/models.py
+++ search_engine/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SearchField(models.Model):   
+    kind = models.CharField(max_length=30)
+    id = models.CharField(primary_key=True, max_length=200)
+    name = models.CharField(max_length=30)
+    createdTime = models.DateTimeField(null = False) 
+    modifiedTime = models.DateTimeField(null = False) 
+    mimeType = models.CharField(max_length=100)
--- search_engine/views.py
+++ search_engine/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import elasticsearch
+from elasticsearch import Elasticsearch
+
 # Create your views here.
-def index():
+def first(request):
+    print(elasticsearch.VERSION)
     return HttpResponse("Hello from search engine")
+    
+def matchStr(string):
+    elastic_client = Elasticsearch(hosts=["192.168.56.101:9200"])
+    result = elastic_client.search(index="drive", body={"query": {"match": {"name": string}}})
+    print ("query hits:", result["hits"]["hits"])
